cc23d6480e4ed905d22a2e93b265cd18f1f3917e
jonathanpuello2007 <121696203+jonathanpuello2007@users.noreply.github.com>
2024-09-26 08:18:19 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+Skip to content
+Commands
+Search
+Recent
+Cloud Services
+Workspace Features
+Collaboration
+Other
+Workspace Extensions
+None installed.
+print("Hello, World!")
+print(1+2)
+print("1+2")
+print("1"+"2")
+print(2.57+3.56)
+
+first_name = input("Please enter your name:")
+middle_initial = input("please enter your middle initial:")
+last_name = input("Please enter your last name:")
+street_address = input ("please enter your street address:")
+
+print (f"Hello, {first_name} {last_name}!")
+
+Run your code
+Results of your code will appear here when you 
+Run
+ the project.
+
+- Replit
